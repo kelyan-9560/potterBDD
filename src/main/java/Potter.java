0981beb45125e3b0fb6965c632b30e8ac492611a
kelyan-9560 <kelyan.bervin@gmail.com>
2022-01-17 11:35:54 +0100

    Refactoring

--- src/main/java/Potter.java
+++ src/main/java/Potter.java
@@ -1,37 +1,99 @@
-import java.util.*;
-
 public class Potter {
 
-    public int book_price = 8;
+    public Potter() {}
 
-    public Map<Integer, Double> discountFor = Map.of(
-            5, 0.25,
-            4, 0.2,
-            3, 0.1,
-            2, 0.05,
-            1, 0.0
-    );
+    public double price(int[] shoppingBasket) {
 
+        int[] countOfBooksArray = {0, 0, 0, 0, 0};
 
+        if (shoppingBasket == null)
+            return 0;
 
-    public double price(List<Integer> books){
-        double price = 0;
+        if (shoppingBasket.length == 1)
+            return 8;
 
-        for (int i = 0; i < books.size(); i++) {
-            //Set<Integer> uniqueBooks = new HashSet<>(books);
-            Set uniqueBooks = new HashSet(books);
-
-
-            price += book_price * uniqueBooks.size() * (1 - discountFor.get(uniqueBooks.size()));
-            /*
-            for(Object item: uniqueBooks){
-                books.remove(books.indexOf(item));
-            }*/
-
-            uniqueBooks.forEach(item -> books.remove(books.indexOf(item)));
+        for (int book : shoppingBasket){
+            countOfBooksArray[book] += 1;
         }
-        return price;
+
+        int maxCount = 0;
+
+        for (int bookCount : countOfBooksArray){
+
+            if (bookCount > maxCount)
+                maxCount = bookCount;
+        }
+
+        int[][] arrayMatrix = new int[5][];
+
+        for (int rowIndex = 0; rowIndex < arrayMatrix.length; rowIndex++)
+        {
+
+            arrayMatrix[rowIndex] = new int[maxCount];
+
+            int bookCountIndex = countOfBooksArray[rowIndex] - 1;
+
+            for (int colunmIndex = 0; colunmIndex < maxCount; colunmIndex++)
+            {
+                if (colunmIndex <= bookCountIndex) {
+                    arrayMatrix[rowIndex][colunmIndex] = rowIndex;
+                } else {
+                    arrayMatrix[rowIndex][colunmIndex] = -1;
+                }
+            }
+        }
+
+        double totalSum = 0;
+
+        boolean previousColumn5 = false;
+
+        for (int colunmIndex = 0; colunmIndex < maxCount; colunmIndex++)
+        {
+            double sumOfColumn = 0;
+
+            double sumOfNextColumn = 0;
+
+            for (int rowIndex = 0; rowIndex < arrayMatrix.length; rowIndex++)
+            {
+                if (arrayMatrix[rowIndex][colunmIndex] > -1) {
+                    sumOfColumn += 1;
+                }
+
+                int nextColumnIndex = maxCount -1;
+
+                if ( colunmIndex < nextColumnIndex) {
+
+                    if (arrayMatrix[rowIndex][colunmIndex + 1] > -1) {
+                        sumOfNextColumn += 1;
+                    }
+                }
+            }
+
+            if ((sumOfColumn == 5)  && (sumOfNextColumn == 3)) {
+                previousColumn5 = true;
+
+                totalSum += (4 * 8 * 0.8) + (4 * 8 * 0.8);
+
+            } else if (sumOfColumn == 5)  {
+                totalSum += (5 * 8 * 0.75);
+            }
+            else if (sumOfColumn == 4)  {
+                totalSum += (4 * 8 * 0.8);
+            }
+            else if (sumOfColumn == 3)  {
+                if (!previousColumn5)
+                    totalSum += (3 * 8 * 0.90);
+                else
+                    previousColumn5 = false;
+            }
+            else if (sumOfColumn == 2)  {
+                totalSum += (2 * 8 * 0.95);
+            }
+            else if (sumOfColumn == 1)  {
+                totalSum += 8;
+            }
+        }
+        return totalSum;
     }
 
-    
 }
--- src/test/java/PotterTest.java
+++ src/test/java/PotterTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +10,7 @@ public class PotterTest {
 
     @Test
     void noBooksShouldCostZero(){
-        List<Integer> books = List.of();
+        int[] books = {};
         assertEquals(potter.price(books), 0);
     }
 
@@ -19,71 +18,68 @@ public class PotterTest {
     //when only identical books
     @Test
     void oneBookShouldCost8(){
-        List<Integer> books = List.of(1);
+        int[] books = {1};
         assertEquals(potter.price(books), 8);
 
-        List<Integer> books2 = List.of(2);
+        int[] books2 = {2};
         assertEquals(potter.price(books2), 8);
 
-        List<Integer> books3 = List.of(3);
+        int[] books3 = {3};
         assertEquals(potter.price(books3), 8);
 
-        List<Integer> books4 = List.of(4);
+        int[] books4 = {4};
         assertEquals(potter.price(books4), 8);
 
-        List<Integer> books5 = List.of(5);
+        int[] books5 = {5};
         assertEquals(potter.price(books5), 8);
     }
 
     @Test
     void twoBooksShouldCost16(){
-        List<Integer> books = List.of(1, 1);
+        int[] books = {1, 1};
         assertEquals(potter.price(books), 16);
     }
 
     @Test
     void oneCollectionWithNoMoreBooks(){
-        List<Integer> books = List.of(1, 2);
+        int[] books = {1, 2};
         assertEquals(potter.price(books), 15.2);
 
-        List<Integer> books2 = List.of(1, 2, 3);
+        int[] books2 = {1, 2, 3};
         assertEquals(potter.price(books2), 21.6);
 
-        List<Integer> books3 = List.of(1, 2, 3, 4);
+        int[] books3 = {1, 2, 3, 4};
         assertEquals(potter.price(books3), 25.6);
 
-        List<Integer> books4 = List.of(1, 2, 3, 4, 5);
+        int[] books4 = {1, 2, 3, 4, 5};
         assertEquals(potter.price(books4), 30);
     }
 
     @Test
     void whenBuyingMultipleCollections(){
         //buying a 3-books collection + a 2-books collection should cost 36.8e
-        List<Integer> books1 = List.of(1, 2, 3, 1, 2);
+        int[] books1 = {1, 2, 3, 1, 2};
         assertEquals(potter.price(books1), 21.6 + 15.2);
 
 
-
         //buying one collection of each (2, 3, 4, 5) + one identical
-        List<Integer> books2 = List.of(
+        int[] books2 = {
                 1, 2, 3, 4, 5,
                 1, 2, 3, 4,
                 1, 2, 3,
                 1, 2,
                 1
-        );
+        };
         assertEquals(potter.price(books2), 30 + 25.6 + 21.6 + 15.2 + 8);
 
         //prefers 2 4-books collections over a 5-books one + a 3-books one
-        List<Integer> books3 = List.of(
+        int[] books3 = {
                 1, 2, 3, 4,
                 2, 3, 4, 5
-        );
+        };
         assertEquals(potter.price(books3), 25.6 * 2);
 
     }
-
-
 
 
 }
