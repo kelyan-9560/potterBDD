72ad2fd3c9149f05d7e14ea715b6fb6d53448589
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-01-17 15:29:19 +0100

    :bug: kata potter qui fonctionne, reste a faire les test bdd

--- src/main/java/Potter.java
+++ src/main/java/Potter.java
@@ -1,99 +1,38 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Potter {
+
+    private final Map<Integer, Double> discountFor = new HashMap<Integer, Double>() {
+        {
+            put(5,0.25);
+            put(4, 0.2);
+            put(3, 0.1);
+            put(2, 0.05);
+            put(1, 0.0);
+            put(0, 0.0);
+        }
+    };
+
+    private final int bookPrice = 8;
 
     public Potter() {}
 
     public double price(int[] shoppingBasket) {
+        double price = 0;
 
-        int[] countOfBooksArray = {0, 0, 0, 0, 0};
+        if(shoppingBasket.length == 0) return price;
 
-        if (shoppingBasket == null)
-            return 0;
+        while(shoppingBasket.length > 0){
+            int[] unique = Arrays.stream(shoppingBasket).distinct().toArray();
 
-        if (shoppingBasket.length == 1)
-            return 8;
+            price += bookPrice * unique.length * (1 - discountFor.get(unique.length));
 
-        for (int book : shoppingBasket){
-            countOfBooksArray[book] += 1;
+            shoppingBasket = removeItem(shoppingBasket,unique);
         }
 
-        int maxCount = 0;
-
-        for (int bookCount : countOfBooksArray){
-
-            if (bookCount > maxCount)
-                maxCount = bookCount;
-        }
-
-        int[][] arrayMatrix = new int[5][];
-
-        for (int rowIndex = 0; rowIndex < arrayMatrix.length; rowIndex++)
-        {
-
-            arrayMatrix[rowIndex] = new int[maxCount];
-
-            int bookCountIndex = countOfBooksArray[rowIndex] - 1;
-
-            for (int colunmIndex = 0; colunmIndex < maxCount; colunmIndex++)
-            {
-                if (colunmIndex <= bookCountIndex) {
-                    arrayMatrix[rowIndex][colunmIndex] = rowIndex;
-                } else {
-                    arrayMatrix[rowIndex][colunmIndex] = -1;
-                }
-            }
-        }
-
-        double totalSum = 0;
-
-        boolean previousColumn5 = false;
-
-        for (int colunmIndex = 0; colunmIndex < maxCount; colunmIndex++)
-        {
-            double sumOfColumn = 0;
-
-            double sumOfNextColumn = 0;
-
-            for (int rowIndex = 0; rowIndex < arrayMatrix.length; rowIndex++)
-            {
-                if (arrayMatrix[rowIndex][colunmIndex] > -1) {
-                    sumOfColumn += 1;
-                }
-
-                int nextColumnIndex = maxCount -1;
-
-                if ( colunmIndex < nextColumnIndex) {
-
-                    if (arrayMatrix[rowIndex][colunmIndex + 1] > -1) {
-                        sumOfNextColumn += 1;
-                    }
-                }
-            }
-
-            if ((sumOfColumn == 5)  && (sumOfNextColumn == 3)) {
-                previousColumn5 = true;
-
-                totalSum += (4 * 8 * 0.8) + (4 * 8 * 0.8);
-
-            } else if (sumOfColumn == 5)  {
-                totalSum += (5 * 8 * 0.75);
-            }
-            else if (sumOfColumn == 4)  {
-                totalSum += (4 * 8 * 0.8);
-            }
-            else if (sumOfColumn == 3)  {
-                if (!previousColumn5)
-                    totalSum += (3 * 8 * 0.90);
-                else
-                    previousColumn5 = false;
-            }
-            else if (sumOfColumn == 2)  {
-                totalSum += (2 * 8 * 0.95);
-            }
-            else if (sumOfColumn == 1)  {
-                totalSum += 8;
-            }
-        }
-        return totalSum;
+        return price;
     }
 
 }
